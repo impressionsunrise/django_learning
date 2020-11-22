d1e3b8940a2b8fd353649290cf8780d021d6340c
Aleksandr Ivanov <shamrockspb@gmail.com>
2020-11-22 17:01:01 +0300

    Changes

--- blog/urls.py
+++ blog/urls.py
@@ -16,10 +16,12 @@ Including another URLconf
 from django.urls import path
 from .views import (
     ArticleDetailView,
+    ArticleCreateView,
     ArticleListView
 )
 app_name = 'articles'
 urlpatterns = [
     path('', ArticleListView.as_view(), name='article-list'),
+    path('create/', ArticleCreateView.as_view(), name='article-create'),
     path('<int:pk>/', ArticleDetailView.as_view(), name='article-detail'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -8,9 +8,13 @@ from django.views.generic import (
 	DeleteView
 )
 
-
+from .forms import ArticleModelForm
 from .models import Article
 
+class ArticleCreateView(CreateView):
+	template_name = 'articles/article_create.html'
+	form_class = ArticleModelForm
+	queryset = Article.objects.all()
 class ArticleListView(ListView):
 	template_name = 'articles/article_list.html'
 	queryset = Article.objects.all()
